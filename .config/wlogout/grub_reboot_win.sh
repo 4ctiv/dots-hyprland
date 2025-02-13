7ecfe87da8012b7120bb2bfa2bde0101ef09a9f8
4ctiv <leif.gittel@protonmail.com>
2025-02-13 21:09:55 +0100

    Started overhauling screenshot hotkeys and tools (not finished want to use flameshot multi-screen) and various other additions & changes

---
+++ .config/wlogout/grub_reboot_win.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Reboots the computer into Windows
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "This script must be run with sudo privilages"
+    exit
+fi
+
+boot_num=$(/usr/bin/efibootmgr | rg "Boot(\d+)\* Windows Boot Manager" -or '$1')
+/usr/bin/efibootmgr -n $boot_num
+reboot
