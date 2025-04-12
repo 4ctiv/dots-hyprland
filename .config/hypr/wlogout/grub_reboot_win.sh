0b95919996bdf98a953dc9c86b8ba22c64b23056
4ctiv <leif.gittel@protonmail.com>
2025-04-12 15:19:36 +0200

    Update hyprland config & scripts
    .

--- .config/hypr/wlogout/grub_reboot_win.sh
+++ .config/hypr/wlogout/grub_reboot_win.sh
@@ -6,14 +6,19 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     exit
 fi
 
+# Settings
+export filter="[Ww]indows"
+
+# Default override
+[ ! -z "$1" ] && filter="$1"
 ### Systemd + efibootmgr method (more stable)
-#boot_num=$(/usr/bin/efibootmgr | rg "Boot(\d+)\* Windows Boot Manager" -or '$1')
+#boot_num=$(/usr/bin/efibootmgr | rg "${filter:?is empty}" -or '$1')
 #systemctl reboot --boot-loader-entry=${boot_num:?entry id is empty}
 
 ### Grub method (consistent user experience)
 grub_num=$(sudo awk -F \' '$1=="menuentry " || $1=="submenu " {print i++ " : " $2}; /\smenuentry / {print "\t" i-1">"j++ " : " $2};' /boot/grub/grub.cfg \
-           | grep -E '[W+w]indows' | awk 'NR==1{print $1}')
-sudo grub-reboot ${grub_num:?entry id is empty} # Set next boot entrie
+           | grep -E "${filter}" | awk 'NR==1{print $1}')
+sudo grub-reboot ${grub_num:?entry id not found} # Set next boot entrie
 sudo reboot
 
 exit 0
