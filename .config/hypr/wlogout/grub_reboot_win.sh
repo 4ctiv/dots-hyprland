53e143906bf45e2a9f727933bb63398d39f767fa
4ctiv <leif.gittel@protonmail.com>
2025-04-12 14:52:02 +0200

    Ollama script now support nvidia > amd > intel > cpu & grub reboot to windows fix

--- .config/hypr/wlogout/grub_reboot_win.sh
+++ .config/hypr/wlogout/grub_reboot_win.sh
@@ -6,5 +6,14 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     exit
 fi
 
-boot_num=$(/usr/bin/efibootmgr | rg "Boot(\d+)\* Windows Boot Manager" -or '$1')
-systemctl reboot
+### Systemd + efibootmgr method (more stable)
+#boot_num=$(/usr/bin/efibootmgr | rg "Boot(\d+)\* Windows Boot Manager" -or '$1')
+#systemctl reboot --boot-loader-entry=${boot_num:?entry id is empty}
+
+### Grub method (consistent user experience)
+grub_num=$(sudo awk -F \' '$1=="menuentry " || $1=="submenu " {print i++ " : " $2}; /\smenuentry / {print "\t" i-1">"j++ " : " $2};' /boot/grub/grub.cfg \
+           | grep -E '[W+w]indows' | awk 'NR==1{print $1}')
+sudo grub-reboot ${grub_num:?entry id is empty} # Set next boot entrie
+sudo reboot
+
+exit 0
