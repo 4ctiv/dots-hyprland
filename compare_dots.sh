b2291ef13a1f891dbb2ce62a82689e33afb9bacd
activ <noreply.activ@example.de>
2025-08-21 00:52:50 +0200

    Add install for meld on diff check script

--- compare_dots.sh
+++ compare_dots.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-# Simple script to use meld for comparison of changed dotfiles
+# This script views possible differences to current dot files
+# NOTE: Dots for same folder have to exist on both ends otherwise they are skipped
 
 USR_CFG="$HOME/.config"
 GIT_CFG="$(pwd)/.config" # script runs in git root
 
+# Install diff tool if required
+[[ -f "$(which meld 2>/dev/null)" ]] || (echo 'Installing "meld"'; sudo pacman -S --needed meld)
+
+# Fix error on absent '.gitignore'
 if [[ ! -f "${GIT_CFG:?git dir not set}/.gitignore" ]];then
   touch "${GIT_CFG:?git dir not set}/.gitignore" # Prevent diff exclude file missing error
 fi
 
+# Compare overlapping dot folders
 diff_dirs=($(diff -q --no-dereference --exclude-from="${GIT_CFG:?git dir not set}/.gitignore" "${USR_CFG:?usr dir not set}" "$GIT_CFG" |\
   awk '/(^Common|^[^Only])/{$1=$2="";split($0,a,/ and /); for (i=1; i<=2; i++) print a[i]}'))
 for ((i = 0; i < ${#diff_dirs}; i+=2)); do
