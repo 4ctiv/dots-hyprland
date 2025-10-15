d8f9d7d9d4ed688d2ba0c87ba50f776094834b3d
Leif G. <leif.gittel@noreply.4ctiv.de>
2025-10-15 15:27:25 +0200

    Update basic configs

--- compare_dots.sh
+++ compare_dots.sh
@@ -2,11 +2,12 @@
 # This script views possible differences to current dot files
 # NOTE: Dots for same folder have to exist on both ends otherwise they are skipped
 
+DIFF_TOOL="meld"
 USR_CFG="$HOME/.config"
-GIT_CFG="$(pwd)/.config" # script runs in git root
+GIT_CFG="$(git rev-parse --show-toplevel)/.config" # script runs in git root
 
 # Install diff tool if required
-[[ -f "$(which meld 2>/dev/null)" ]] || (echo 'Installing "meld"'; sudo pacman -S --needed meld)
+[[ -f "$(which "${DIFF_TOOL:?not set}" 2>/dev/null)" ]] || (echo "Installing ${DIFF_TOOL}"; sudo pacman -S --needed "${DIFF_TOOL:?not set}")
 
 # Fix error on absent '.gitignore'
 if [[ ! -f "${GIT_CFG:?git dir not set}/.gitignore" ]];then
@@ -18,6 +19,6 @@ diff_dirs=($(diff -q --no-dereference --exclude-from="${GIT_CFG:?git dir not set
   awk '/(^Common|^[^Only])/{$1=$2="";split($0,a,/ and /); for (i=1; i<=2; i++) print a[i]}'))
 for ((i = 0; i < ${#diff_dirs}; i+=2)); do
   echo "----"
-  echo meld "${diff_dirs[i]}" "${diff_dirs[i+1]}"
-  meld -n "${diff_dirs[i]}" "${diff_dirs[i+1]}" &
+  echo "$DIFF_TOOL ${diff_dirs[i]} ${diff_dirs[i+1]}"
+  ${DIFF_TOOL} -n "${diff_dirs[i]}" "${diff_dirs[i+1]}" &
 done
