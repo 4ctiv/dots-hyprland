ec97cc0d7eb7ed103cc83ec25f527c934a2443d4
activ <noreply.activ@example.de>
2025-10-25 14:04:24 +0200

    Small reorganisation of fish alias file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,52 +12,54 @@ if status is-interactive
     # fish_config theme choose "Rosé\ Pine"
     # set fish_color_... "0xfffffff"
 
-    ########################
-    ### Typo redirection ###
-    ########################
+    ##########################
+    ### (Typo) redirection ###
+    ##########################
+    # common typo redirection
     alias cd..='cd ..'
-    alias nano='vim'
-    alias udpate='sudo pacman -Syyu'
-    alias upate=' sudo pacman -Syyu'
-    alias updte=' sudo pacman -Syyu'
-    alias updqte='sudo pacman -Syyu'
     alias claer="clear"
-    alias gtop="btop"
-    alias neofetch="fastfetch -l .config/neofetch/great-wave-transparent-2.png --logo-height 20"
-    #windows commands
+    # windows command redirection
     alias dir="ls"
     alias taskmgr="htop"
     alias ipconfig="ip a"
     alias diskmgmt="sudo cfdisk"
+    # linux   command redirection
+    alias nano="vim"
+    alias gtop="btop"
+    alias podman="docker"
+    alias neofetch="fastfetch -l .config/neofetch/great-wave-transparent-2.png --logo-height 20"
 
     #################################################
     ### Aliases for Input & Basic system settings ###
     #################################################
-    # keyboard
-    alias set-keyboard-layout-qwerty-de="sudo localectl set-x11-keymap de"
-    alias set-keyboard-layout-qwerty-us="sudo localectl set-x11-keymap us"
-    # locale (language, font)
+    # font
+    alias update-font='sudo fc-cache -fv'
+    # keyboard & language
+    alias set-keyboard-de="sudo localectl set-x11-keymap de"
+    alias set-keyboard-us="sudo localectl set-x11-keymap us"
     alias set-locale-de="sudo localectl set-locale LANG=de_DE.UTF-8"
     alias set-locale-us="sudo localectl set-locale LANG=en_US.UTF-8"
-    # systemd: https://www.tecmint.com/disable-suspend-and-hibernation-in-linux
+    # systemd
     alias hibernation-enable="sudo systemctl unmask hibernate.target"
     alias hibernation-disable="sudo systemctl mask hibernate.target"
+    alias update-systemctl="sudo sysctl --system"
+    # grub
+    alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+    alias grub-install-efi-tpm="sudo grub-install --target=x86_64-efi --efi-directory=/boot/efi --modules='tpm' --disable-shim-lock"
 
     ###########################
     ### Software Management ###
     ###########################
     # should be defaults (in ineractive mode)
     alias wget="wget -c"
-    # processes
     alias process-tree="ps auxf"
-    # debug
-    alias check-io="sudo iotop -o -a"
-    alias check-dump="coredumpctl gdb"
-    alias check-hex-ascii="hexdump -C"
-    #yt-dpl (youtube)
+    # debugging commands
+    alias check-coredump="coredumpctl gdb"
+    alias check-file-hex-ascii="hexdump -C"
+    # yt-dpl (youtube)
     alias yt-audio="yt-dlp --extract-audio --audio-format best "
     alias yt-video="yt-dlp -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 "
-    #web
+    # web
     alias web-app="brave --app"
     alias unhblock="hblock -S none -D none"
     # environemnts
@@ -67,6 +69,7 @@ if status is-interactive
     ###############################
     ### Beautify terminal apps  ###
     ###############################
+    alias print-devider="printf '%*s\n' (tput cols) '' | tr ' '" # call with devider char
     # Colorize (may make output not grep-able)
     alias ip="ip -c"
     alias ls='ls --color=auto'
@@ -84,36 +87,31 @@ if status is-interactive
     #########################
     alias yay="paru"
     alias free="free -mt"
-    alias gksudo="pkexec" # policy kit (polkit)
-    ##################
-    ## Update Stuff ##
-    ##################
-    alias update-font='sudo fc-cache -fv'
-    alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
-    alias grub-install-efi-tpm="sudo grub-install --target=x86_64-efi --efi-directory=/boot/efi --modules='tpm' --disable-shim-lock"
+    alias gksudo="pkexec"
     #################
     ## Check Stuff ##
     #################
-    alias check-audio="pactl info | grep 'Server Name'" #check audio (pulseaudio or pipewire)
-    alias check-cpu="cpuid -i | grep uarch | head -n 1" #check cpu
-    alias check-disk-speed="sudo hdparm -tT"
+    # basic hw/sw checks
     alias check-hw="hwinfo --short" #check hardware (short summary)
-    alias check-mc='grep . /sys/devices/system/cpu/vulnerabilities/*' #check microcode vulnerabilities
-    alias check-memory="free -mt"
-    alias check-system-probe="sudo -E hw-probe -check -minimal -all -show"
-    #alias check-fish-start="time fish --profile-startup /tmp/startup.prof -c exit && sort -nk2 /tmp/startup.prof | less"
+    alias check-hw-cpu="cpuid -i | grep uarch | head -n 1"
+    alias check-hw-disk-io="sudo iotop -o -a"
+    alias check-hw-disk-speed="sudo hdparm -tT"
+    alias check-hw-memory="free -mt"
+    alias check-sw-audio="pactl info | grep 'Server Name'" # (pulseaudio or pipewire)
+    alias check-sw-microcode='grep . /sys/devices/system/cpu/vulnerabilities/*' # microcode vulnerabilities
+    alias check-sw-probe="sudo -E hw-probe -check -minimal -all -show"
     ################
     ## List Stuff ##
     ################
     # Logs
-    alias log-system="sudo dmesg; sudo journalctl;"
+    alias log-system="printf 'Showing logs of last 10 minutes\n'; sudo journalctl --no-page --since='10 minutes ago' && printf '\n%*s\n\n' (tput cols) '' | tr ' ' '=' && sudo dmesg --since '10 minutes ago'"
     alias log-system-now="sudo dmesg --follow-new &; sudo journalctl --follow --since 'now' &;"
     alias log-systemd-user="systemctl status systemd-logind.service ; systemctl status user@$(id -u).service"
-    alias log-calamares="cat /var/log/Calamares.log"
-    alias log-pacman="   cat /var/log/pacman.log"
-    alias log-xorg="     cat /var/log/Xorg.0.log"
-    alias log-xorg-old=" cat /var/log/Xorg.0.log.old"
-    alias log-sddm="     cat ~/.local/share/sddm/*.log"
+    alias log-calamares="tail -f /var/log/Calamares.log"
+    alias log-pacman="   tail -f /var/log/pacman.log"
+    alias log-xorg="     tail -f /var/log/Xorg.0.log"
+    alias log-xorg-old=" tail -f /var/log/Xorg.0.log.old"
+    alias log-sddm="     tail -f ~/.local/share/sddm/*.log"
     alias log-notifications="makoctl history # | jq '.data[0][].body.data'"
     # System info
     alias list-system-usage="top -u nobody -bn1 | head -n 5"
@@ -133,40 +131,38 @@ if status is-interactive
     alias list-net-port="netstat -tunlp | sort -t: -k4 -n | grep LISTEN"
     # Packages
     # HOW 2 (EXTENDED USAGE): pacman -S --needed - < my-list-of-packages.txt # E.g. install packages from a list
-    alias list-packs="         sudo pacman -Qqe" # List of explicitly installed packages
-    alias list-packs-repo="    sudo paclist"
+    alias list-packs="sudo pacman -Qqe" # List of explicitly installed packages
+    alias list-packs-info="pacman -Sii"
+    alias list-packs-repo="sudo paclist"
+    alias list-packs-aur="sudo pacman -Qqem" # List of AUR packages
     alias list-packs-explicit="sudo pacman -Qqet" # List of explicitly installed packages without deps
-    alias list-packs-aur="     sudo pacman -Qqem" # List of AUR packages
-    alias list-packs-broken="  sudo pacman -Qk 2>/dev/null | grep -v ' 0 missing files' | cut -d: -f1"
-    alias list-packs-size="  expac -H M '%m\t%n' | sort -h | nl" # List of package sizes
+    alias list-packs-size="expac -H M '%m\t%n' | sort -h | nl" # List of package sizes
+    alias list-packs-owner='pacman -Qo'
     alias list-packs-recent="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
     alias list-packs-recent-extended="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -3000 | nl"
     alias list-packs-desktop="find (string split ':' $XDG_DATA_DIRS)/applications -type f -name '*.desktop' 2>/dev/null"
     alias list-packs-desktop-v2="ls (string split ':' $XDG_DATA_DIRS)/applications"
+    alias list-packs-broken="sudo pacman -Qk 2>/dev/null | grep -v ' 0 missing files' | cut -d: -f1"
     alias list-packs-outdated="checkupdates"
-    alias list-pack-owner='pacman -Qo'
-    alias list-pack-info=' pacman -Sii'
+
 
     ##########################
     ### Software managment ###
     ##########################
     alias packs-add='    sudo pacman -S'
-    alias packs-remove=' sudo pacman -R'
-    alias packs-install='  packs-add'
+    alias packs-install='packs-add'
+    alias packs-remove=' sudo pacman -Rns'
     alias packs-uninstall='packs-remove'
     alias packs-owner='  sudo pacman -Qo'
     alias packs-deps='   sudo pacman -Sii'
     alias packs-update=' sudo pacman -Syyu'
-    alias packs-as-explicit=' pacman -D --asexplicit'
-    alias packs-as-dependecy='pacman -D --asdeps'
-    alias packs-cleanup='sudo pacman -Rns $(pacman -Qtdq)' # Cleanup orphaned packages
-    alias packs-missing="     pacman -Dkk"
-    #skip integrity check (unsave !!!)
-    alias yay-skip='yay -S --mflags --skipinteg'
-    #verify signatures
-    #function check-hash $ALGO $CSM $FILE
+    alias packs-cleanup='pacman -Scc && sudo pacman -Rns $(pacman -Qqtd)' # Cleanup orphaned packages
+    alias packs-missing="pacman -Dkk"
+    alias packs-set-explicit=' pacman -D --asexplicit'
+    alias packs-set-dependecy='pacman -D --asdeps'
+    # verify signatures
     alias check-gpg="gpg2 --keyserver-options auto-key-retrieve --verify"
-    alias update-systemctl="sudo sysctl --system"
+    alias check-ssh="ssh -T"
 
     #########################
     ### Aliases for fixes ###
@@ -191,26 +187,27 @@ if status is-interactive
     alias fix-conf-grub="/usr/local/bin/arcolinux-fix-grub"
     alias fix-conf-pacman="         /usr/local/bin/arcolinux-fix-pacman-conf"
     alias fix-conf-sddm="/usr/local/bin/arcolinux-fix-sddm-config"
+    ### default config restore
     alias reset-vim=' cp /etc/skel/.vimrc ~/.vimrc && rm -i ~/.vim && cp /etc/skel/.vim ~/.vim && echo "Restored vim"'
-    alias reset-zsh=' cp /etc/skel/.zshrc                   ~/.zshrc                   && echo "Restored zsh"'
-    alias reset-bash='cp /etc/skel/.bashrc                  ~/.bashrc                  && echo "Restored bash"'
-    alias reset-fish='cp /etc/skel/.config/fish/config.fish ~/.config/fish/config.fish && echo "Restored fish"'
+    alias reset-zsh=' cp /etc/skel/.zshrc                   ~/.zshrc                           && echo "Restored zsh"'
+    alias reset-bash='cp /etc/skel/.bashrc                  ~/.bashrc                          && echo "Restored bash"'
+    alias reset-fish='cp /etc/skel/.config/fish/config.fish ~/.config/fish/config.fish         && echo "Restored fish"'
 
     ##############################
     ### File System Maintenace ###
     ##############################
-    # Filesystem
-    alias list-partitions="lsblk -o name,label,fstype,fsuse%,uuid"
+    alias list-file-info="stat"
+    alias list-file-size="sudo du -shc"
+    alias list-file-mime="file --mime-type"
+    alias list-drive-ids="lsblk -io KNAME,TYPE,SIZE,MODEL,LABEL | awk '\$2 != \"loop\" { print }'"
+    alias list-drive-size="df -h"
     alias list-drive-busy="lsof -R"
     alias list-drive-mounts="df -h | grep -E --color=never '/dev/[^(loop|dm\-0)]|//'"
+    alias list-drive-partitions="lsblk -o name,label,fstype,fsuse%,RO,HOTPLUG -e7"
     alias list-btrfs-snapshots="sudo timeshift --list"
     alias list-btrfs-filesystem="sudo btrfs filesystem df /"
     alias list-btrfs-subvolumes="sudo btrfs su li -t /"
     alias btrfs-compress="sudo btrfs filesystem defragment -r -v -czstd"
     alias btrfs-balance="sudo btrfs balance start -dusage=66"
-    alias list-size-drive="df -h"
-    alias list-size-file="sudo du -shc"
-    alias check-file-info="stat"
-    alias list-file-mime="file --mime-type"
 
 end # End of interactive prompt config
